91679009c6d5e0007f6a0ef0818785442027d300
Wesly Daud <125133290+Wesly16@users.noreply.github.com>
2023-08-22 20:33:33 +0700

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -18,7 +18,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            <br /> I am highly motivated to learn new experiences, enjoy activities that involve a lot of people or teamwork And have an interest in web programming and data.
+            <br /> I am highly motivated to learn new experiences, enjoy activities that involve a lot of people or teamwork And have an interest in web programming and data science.
             <br />
             Currently, I am still pursuing my undergraduate education at Universitas Dinamika Bangsa by majoring in information systems. I am currently in the 6th semester and am in the lecture period.
           </p>
